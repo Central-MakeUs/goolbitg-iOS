1f9ce6e4c34f510eb5331048efd0c198fedc9b39
Kim JaeHyung <you3192001@icloud.com>
2025-01-04 23:07:17 +0900

    Initial Commit

---
+++ Goolbitg-iOS/Goolbitg_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Goolbitg_iOSApp.swift
+//  Goolbitg-iOS
+//
+//  Created by Jae hyung Kim on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Goolbitg_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
