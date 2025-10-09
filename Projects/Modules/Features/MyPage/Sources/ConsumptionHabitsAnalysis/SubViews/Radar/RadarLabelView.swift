ffa8df36172b5b639ee2074687db91188ce2ca14
Kim JaeHyung <you3192001@icloud.com>
2025-10-09 23:36:14 +0900

    [Feat] Radar 차트 뷰에 사용할 텍스트 레이블 영역

---
+++ Projects/Modules/Features/MyPage/Sources/ConsumptionHabitsAnalysis/SubViews/Radar/RadarLabelView.swift
@@ -0,0 +1,43 @@
+//
+//  RadarLabelView.swift
+//  FeatureMyPage
+//
+//  Created by Jae hyung Kim on 10/8/25.
+//
+
+import SwiftUI
+import Utils
+
+// MARK: - 라벨 뷰
+struct RadarLabel: View {
+    /// 항목 이름
+    let name: String
+    /// 현재 값
+    let current: String
+    /// 최댓 값
+    let maximum: String
+    /// 하이라이트 여부
+    let highlight: Bool
+
+    var body: some View {
+        VStack(spacing: 4) {
+            Text(name)
+                .font(FontHelper.body5.font)
+                .foregroundStyle(GBColor.grey200.asColor)
+            
+            HStack(spacing: 0) {
+                Text(current)
+                    .font(FontHelper.body5.font)
+                    .foregroundStyle(.white)
+                
+                Text("/")
+                    .font(FontHelper.body5.font)
+                    .foregroundStyle(GBColor.grey400.asColor)
+                
+                Text(maximum)
+                    .font(FontHelper.body5.font)
+                    .foregroundStyle(GBColor.grey400.asColor)
+            }
+        }
+    }
+}
